f90a9e699542853bb4a5e5bc09231a67807922bf
vkorotkikh <va.korotki@gmail.com>
2017-10-21 20:08:37 -0400

    Update

--- fx_mpgadgets.py
+++ fx_mpgadgets.py
@@ -46,7 +46,7 @@ def mp_gadgetcalc_abonly(abcoef_list, mpcount=64, numpaks=8):
 		# print("Length ab ", len(abcoefpak))
 		print("Pack/Slice: ", pak, ":", islice)
 		for ind in range(0,paksize):
-			if ind == 251:
+			if ind == 580:
 				exit()
 			adjadinknum = ind + pak
 			print("Adinkra:", adjadinknum)
